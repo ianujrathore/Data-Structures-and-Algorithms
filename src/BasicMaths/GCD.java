7fac5e79c9ab67304082833b6771637911a12f8c
ianujrathore <ianujrathore@gmail.com>
2026-04-01 22:38:45 +0530

    Added GCD Program - Euclidean Algorithm

---
+++ src/BasicMaths/GCD.java
@@ -0,0 +1,18 @@
+package BasicMaths;
+
+import java.util.Scanner;
+
+public class GCD {
+    public static void main(String [] args){
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        while(a > 0 && b > 0){
+            if (a > b) a = a % b;
+            else b = b % a;
+        }
+        if(a==0) System.out.println(b);
+        System.out.println(a);
+    }
+}
